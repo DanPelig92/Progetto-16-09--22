c702982c979bace7f179ea6cd984b69083106576
Dan <Danliguriadigitale@gmail.com>
2022-09-16 16:00:13 +0200

    Facce del dado!

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
 window.console.log("Ho fatto il cazzo di caffè!");
-document.write("Ho fatto il caffè" + "<br>");
-var Tazzina = 0;
-let Nuovatazzina = 0;
-const Caffè = 23; //Commento?//
-/*Commento C like */
-let lattuga = 0;
-let cipolla = 1;
-let banane= 'Gianni';
-let zucchero = true;
-let br = "<br>"
-
-if (cipolla > 2) {
-    zucchero = false;
-} else {
-    cipolla++ //cipolla = cipolla+1 //
-}
-
-document.write(cipolla + br);
-
-for (let i = 0; i < 100; i++){
-lattuga++;
-}
-
-while (lattuga < 102 ) {
-    cipolla++;
-    lattuga++;
-}
-
-let aglio = 1 + 3;
-let melanzana = prompt ("Scrivi qui");
-document.write(melanzana);
+let Facce = prompt ("Facce del dado");
+let Casuale = (Math.floor(Math.random() * Facce)+1);
+document.write(Casuale)
