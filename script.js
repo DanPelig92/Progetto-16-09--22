919341c4eed633e7e9d68333b4e8a100cb0a6ed7
Dan <Danliguriadigitale@gmail.com>
2022-09-16 12:02:56 +0200

    Corretto Html

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-Window.console.log("Ho fatto il cazzo di caffè!");
+window.console.log("Ho fatto il cazzo di caffè!");
 document.write("Ho fatto il caffè");
 var Tazzina = 0;
 let Nuovatazzina = 0;
-const Caffè = 23;
+const Caffè = 23; //Commento?//
+/*Commento C like */
+let lattuga = 0;
+let cipolla = 0.989;
+let banane= 'Gianni';
+let zucchero = true;
+
+if (cipolla > 2) {
+    zucchero = false;
+} else {
+    cipolla++ //cipolla = cipolla+1 //
+}
+
+document.write(cipolla);
