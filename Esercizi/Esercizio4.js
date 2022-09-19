a25413e8846b80aa9b2dc3f14434bb1088f719ac
Dan <Danliguriadigitale@gmail.com>
2022-09-19 14:02:01 +0200

    Finito esercizio 4

--- Esercizi/Esercizio4.js
+++ Esercizi/Esercizio4.js
@@ -1,5 +1,11 @@
 const prompt = require('prompt-sync')(); /*installlare "npm install prompt-sync"*/ /*ricordati node script.js */
 let numero = prompt ("Inserisci un numero: ");
-let intero = parseInt(numero)
-let somma = intero+1
-console.log ("La somma richiesta è: " + somma);
+
+let intero = parseInt(numero);
+
+let somma = 0;
+
+for (let i=0; i<=intero; i++){
+    somma = somma + i
+};
+console.log(somma);
