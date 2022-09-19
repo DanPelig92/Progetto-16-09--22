c2db404c5a85013aa8384417013d4035a95370d2
Dan <Danliguriadigitale@gmail.com>
2022-09-19 14:50:54 +0200

    Esercizio 6 finito, circa

--- Esercizi/Esercizio6.js
+++ Esercizi/Esercizio6.js
@@ -1,1 +1,21 @@
-onst prompt = require('prompt-sync')(); /*installlare "npm install prompt-sync"*/ /*ricordati node script.js */
+const prompt = require('prompt-sync')(); /*installlare "npm install prompt-sync"*/ /*ricordati node script.js */
+let input1= prompt ("Scrivi un numero: ");
+let numero1 = parseInt(input1);
+let input2= prompt ("Scrivi un altro numero: ");
+let numero2 = parseInt(input2);
+let operatore = prompt ("Scrivi l'operatore desiderato (+ - * /)");
+if (operatore == "+") {
+    let operazione = numero1 + numero2; console.log(operazione);
+} else {};
+
+if (operatore == "-") {
+    let operazione = numero1 - numero2; console.log(operazione);
+} else {};
+
+if (operatore == "*") {
+    let operazione = numero1 * numero2; console.log(operazione);
+} else {};
+
+if (operatore == "/") {
+    let operazione = numero1 / numero2; console.log(operazione);
+} else {};
