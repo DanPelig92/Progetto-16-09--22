911750131a1b993a9e683f50e52497fd2a915dc8
Dan <Danliguriadigitale@gmail.com>
2022-09-19 15:40:54 +0200

    Committo per completezza: esercizi 8 e 9 pressoché vuoti, 10 mai iniziato. P.S. 2 e 3 hanno i bonus.

---
+++ Esercizi/Esercizio8.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')(); /*installlare "npm install prompt-sync"*/ /*ricordati node script.js */
+let numero = prompt ("Inserisci un numero: ")
+let intero = parseInt(numero);
+
+for (let i=1; i<=intero; i++){console.log(i)};
---
+++ Esercizi/Esercizio9.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')(); /*installlare "npm install prompt-sync"*/ /*ricordati node script.js */
+let lista = ["1", "2", "3", "4", "5"];
+let risposta = prompt("Indovina la parola: ")
+if (risposta 
